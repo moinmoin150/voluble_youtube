62208f57a3d4fb963f29e657ad9a03e34f90dfc3
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2023-03-09 18:25:43 -0600

    Update download_youtube.py

--- download_youtube.py
+++ download_youtube.py
@@ -3,10 +3,10 @@ import pandas as pd
 import streamlit.components as stc
 import base64
 import time
-import datetime
-import requests
-import tweepy
-import math
+from googleapiclient import discovery
+
+
+
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
@@ -17,21 +17,91 @@ class FileDownloader(object):
 		self.filename = filename
 		self.file_ext = file_ext
 
-	def download_id(self):
+	def download_data(self):
 		b64 = base64.b64encode(self.data.encode()).decode()
 		new_filename = "{}_{}_.{}".format(self.filename,timestr,self.file_ext)
 		st.markdown("#### Download Video Data ###")
 		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here</a>'
 		st.markdown(href,unsafe_allow_html=True)
-		
-	def download_dta(self):
-		b64 = base64.b64encode(self.data.encode()).decode()
-		new_filename = "{}_{}_.{}".format(self.filename,timestr,self.file_ext)
-		st.markdown("#### Download Tweets ###")
-		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here</a>'
-		st.markdown(href,unsafe_allow_html=True)
     
  
-api_key= st.secrets["password"]
+api_key= st.secrets["api_key"]
 youtube = discovery.build('youtube', 'v3', developerKey=api_key)
+
+
+query = st.text_input("Search YouTube")
+order_options = ['date','rating','title','videoCount','viewCount']
+selected_order = st.selectbox("Select an Order", order_options)
+
+search_btn = st.button("Search!", key='search')
+if st.session_state.get('button') != True:
+    st.session_state['button'] = search_btn
+if st.session_state['button'] == True:
+	nextToken = None
+	while True:
+		response = youtube.search().list(
+			q=query, 
+			order=selected_order,
+			part='id', 
+			type='video', 
+			maxResults=50, 
+			pageToken=nextToken
+		).execute()
+
+		try:
+			nextToken = response['nextPageToken']
+		except:
+			break
+
+		if len(response['items']) > 1000:
+			break
+	st.write('Found ', len(response['items']), ' results')
+	
+	ids = [i['id']['videoId'] for i in response['items']]
+	response = youtube.videos().list(
+		part='statistics, snippet',
+		id=ids
+	).execute()
+
+	videoId = [i['id'] for i in response['items']]
+	channelId = [i['snippet']['channelId'] for i in response['items']]
+	title = [i['snippet']['title'] for i in response['items']]
+	publishedAt = [i['snippet']['publishedAt'] for i in response['items']]
+	description = [i['snippet']['description'] for i in response['items']]
+	views = [i['statistics']['viewCount'] for i in response['items']]
+	likes = [i['statistics']['likeCount'] for i in response['items']]
+	comments = [i['statistics']['commentCount'] for i in response['items']]
+	favorites = [i['statistics']['favoriteCount'] for i in response['items']]
+
+	df = pd.DataFrame({
+	    'videoId':videoId,
+	    'channelId':channelId,
+	    'URL':['https://www.youtube.com/watch?v='+str(i) for i in videoId],
+	    'title':title,
+	    'publishedAt':publishedAt,
+	    'description':description,
+	    'views':views,
+	    'likes':likes,
+	    'comments':comments,
+	    'favorites':favorites
+	})
+	
+	st.subheader("Preview first 50 rows:")
+        st.dataframe(df.head(50))
+        download = FileDownloader(df.to_csv(),file_ext='csv').download_data()
+	
+	
+	
+
+
+	
+	
+
+
+
+
+
+
+
+
 
